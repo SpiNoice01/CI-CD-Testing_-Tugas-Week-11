fe987437286aede5b96c55ac221512e06c7af3e3
SpiNoice01 <fadhilaagil72@gmail.com>
2025-06-01 05:14:16 +0700

    Purposfully Make The Test Error

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -2,25 +2,25 @@
 public class Counter {
 
 	private int count;
-	
+
 	public Counter() {
 		this.reset();
 	}
-		
+
 	public void reset() {
-		count = 0;
+		count = 1;
 	}
-	
+
 	public void increment() {
 		count++;
 	}
-	
+
 	public void decrement() {
 		count--;
 	}
-	
+
 	public int getCount() {
 		return count;
 	}
-	
+
 }
